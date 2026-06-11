da6346fbe8456ab0f72ec809c20a7f9d6eac08b7
Sajal Kumar <isajalkumar247@gmail.com>
2026-06-12 00:24:35 +0530

    Remove aurora background animation from visitor screen
    Revert to a clean solid doorway background while keeping the typing welcome and loader.

--- src/components/VisitorScreen.jsx
+++ src/components/VisitorScreen.jsx
@@ -1,12 +1,7 @@
-import AuroraBackground from './AuroraBackground'
-
 export default function VisitorScreen({ children, className = '' }) {
   return (
-    <div className="relative min-h-screen overflow-hidden bg-[#F4F4F1]">
-      <AuroraBackground />
-      <div className={`relative z-10 flex min-h-screen items-center justify-center ${className}`}>
-        {children}
-      </div>
+    <div className={`flex min-h-screen items-center justify-center bg-[#F4F4F1] ${className}`}>
+      {children}
     </div>
   )
 }
